bb2745e6f813f0c1953258836f014eefd9d0814a
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-11-23 12:10:18 +0000

    Deploy collective/gatsby-source-plone to github.com/collective/gatsby-source-plone.git:gh-pages

---
+++ component---src-pages-404-js-f0e1b6146b31df609448.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{84:function(e,t,u){"use strict";var n=u(3);Object.defineProperty(t,"__esModule",{value:!0}),t.default=void 0;var a=n(u(0)),l=n(u(95));t.default=function(){return a.default.createElement(l.default,{title:"404 Not Found"},a.default.createElement("h1",null,"NOT FOUND"),a.default.createElement("p",null,"You just hit a route that doesn't exist... the sadness."))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-f0e1b6146b31df609448.js.map
